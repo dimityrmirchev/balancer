af7f637457ab4254b5dfcb210aa475495d432bca
dimityrmirchev <dimityrmirchev@gmail.com>
2021-03-04 11:13:07 +0200

    requests balanced among backends

---
+++ backendPool.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"net/http/httputil"
+	"sync"
+)
+
+type backendPool struct {
+	backends []backend
+	current  int
+	mutex    *sync.RWMutex
+}
+
+func (pool *backendPool) next() *httputil.ReverseProxy {
+	size := len(pool.backends)
+	if size == 0 {
+		return nil
+	}
+
+	if size == 1 && pool.backends[0].isAlive {
+		return pool.backends[0].proxy
+	}
+
+	pool.mutex.Lock()
+	defer pool.mutex.Unlock()
+	ind := pool.current
+
+	// traverse from current to end
+	for i := ind; i < size; i++ {
+
+		if pool.backends[i].isAlive {
+			if pool.current+1 < size {
+				pool.current = pool.current + 1
+			} else {
+				pool.current = 0
+			}
+
+			return pool.backends[i].proxy
+		}
+		pool.current++
+	}
+
+	// traverse from start to current in case we did not find an alive backend service
+	pool.current = 0
+	for i := 0; i < ind; i++ {
+		if pool.backends[i].isAlive {
+			if pool.current+1 < size {
+				pool.current = pool.current + 1
+			} else {
+				pool.current = 0
+			}
+
+			return pool.backends[i].proxy
+		}
+
+		pool.current++
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,28 +1,55 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"net/http"
+	"net/http/httputil"
 	"net/url"
+	"sync"
 )
 
+var pool backendPool
+
 func main() {
+	port := flag.Int("port", 3001, "port to listen on")
+	flag.Parse()
 	config, err := readConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	backends := parseBackends(config.Backends)
+	pool = backendPool{backends: backends, mutex: new(sync.RWMutex)}
+	server := http.Server{
+		Addr:    ":" + fmt.Sprint(*port),
+		Handler: http.HandlerFunc(balance),
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func balance(w http.ResponseWriter, req *http.Request) {
+	proxy := pool.next()
+	proxy.ServeHTTP(w, req)
+}
+
+func parseBackends(urls []string) []backend {
 	backends := []backend{}
-	for _, conf := range config.Backends {
+	for _, conf := range urls {
 		res, err := url.Parse(conf)
 		if err == nil {
 			backend := backend{}
 			backend.isAlive = true
 			backend.url = res
+			backend.proxy = httputil.NewSingleHostReverseProxy(res)
 			backends = append(backends, backend)
 		}
 
 	}
-
-	//pool := pool{backends: backends, mutex: new(sync.RWMutex)}
-
+	return backends
 }
